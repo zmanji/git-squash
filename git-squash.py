6ee80ee9481541f7f2a7d3a669f0e4fe02cf8082
Zameer Manji <zmanji@gmail.com>
2016-08-28 16:58:14 -0700

    WIP.

--- git-squash.py
+++ git-squash.py
@@ -11,9 +11,11 @@ def main(args):
 
     # TODO(zmanji): Check if the git tree is dirty or not
 
-    # Get the merge base
+    # Get the merge base: `git merge-base <branch> HEAD`
+    # TODO(zmanji): What if the merge-base is HEAD? (no-op?)
     # Get all of the commits between the current branch and the merge base
-    # Soft reset to the merge base?
+    # `git log HEAD..<merge-base>`
+    # Soft reset to the merge base? (just change HEAD to merge_base)
     # Double check the tree is dirty
     # Now commit the changes with the squash message
 
