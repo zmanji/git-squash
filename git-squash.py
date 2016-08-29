bc7c2b8d2a6ea8f32ebe1c1d27af6badc775a3fa
Zameer Manji <zmanji@gmail.com>
2016-08-28 17:41:14 -0700

    Progress.

--- git-squash.py
+++ git-squash.py
@@ -17,14 +17,23 @@ def main(args):
         print("The repo is dirty, please stash or commit changes")
         return 1
 
-    # Get the merge base: `git merge-base <branch> HEAD`
-
+    # Find all commits betweent HEAD and branch
     mb = repo.merge_base(args.branch, "HEAD")
+    if len(mb) == 1:
+        mb = mb[0]
+    else:
+        print("No merge base between current branch and %s found!" % branch)
+        return 1
+
     print(mb)
 
     # TODO(zmanji): What if the merge-base is HEAD? (no-op?)
     # Get all of the commits between the current branch and the merge base
     # `git log HEAD..<merge-base>`
+
+    commits_to_squash = repo.iter_commits(rev='HEAD..%s' % mb.binsha)
+    print(list(commits_to_squash))
+
     # Soft reset to the merge base? (just change HEAD to merge_base)
     # Double check the tree is dirty
     # Now commit the changes with the squash message
