4952117447962ae76a2a14585c6c2c7ae3965f0a
Zameer Manji <zmanji@gmail.com>
2021-06-11 20:50:05 -0400

    move to python3

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='git-squash',
-    version='0.0.1',
-    description='Git utility for squashing all commits in a feature branch.',
-    author='Zameer Manji',
-    license='MIT',
-    py_modules=["git_squash"],
-    install_requires=[
-        "GitPython",
-    ],
-    entry_points={
-        'console_scripts': [
-            'git-squash=git_squash:main'
-        ],
-    }
-)
+setuptools.setup()
